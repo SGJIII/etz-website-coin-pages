6b0747b7e382614093783a013d2ef744d9dfbbfd
Aleksei Gorkov <aleksei.gorkov@mercurydevelopment.com>
2023-09-14 17:18:11 +0700

    [ETZ-3818] Fix google analytics

--- src/js/version.js
+++ src/js/version.js
@@ -4,16 +4,16 @@ import config from "../../package.json";
 console.group("App info:");
 
 window.version = config.version;
-if (process.env.BIULD_STATUS === undefined) {
+if (process.env.BUILD_STATUS === undefined) {
   window.version = "NOT FOR TESTING";
 }
 
-if (process.env.BIULD_STATUS !== "PRODUCTION") {
+if (process.env.BUILD_STATUS !== "PRODUCTION") {
   const label = document.createElement("span");
   label.innerText = window.version;
   label.classList.add("version");
 
-  if (process.env.BIULD_STATUS === undefined) {
+  if (process.env.BUILD_STATUS === undefined) {
     label.classList.add("version__highlight");
   }
   document.body.appendChild(label);
